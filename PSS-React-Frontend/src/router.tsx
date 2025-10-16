f200c474a933b4fb3dca330a143009f69b36b040
Sajjad Islam <s.i.sajjad.islam@gmail.com>
2025-10-16 14:25:09 -0500

    deployment update

--- PSS-React-Frontend/src/router.tsx
+++ PSS-React-Frontend/src/router.tsx
@@ -3,8 +3,20 @@ import { LoginPage } from './modules/auth/LoginPage'
 import { HomePage } from './modules/home/HomePage'
 import { ProtectedRoute } from './shared/ProtectedRoute'
 
-export const router = createBrowserRouter([
-	{ path: '/', element: <LoginPage /> },
-	{ path: '/home', element: <ProtectedRoute><HomePage /></ProtectedRoute> },
-])
-
+// This makes sure routing works both locally ("/") and on GitHub Pages ("/PSS-LLM-Evaluation/")
+export const router = createBrowserRouter(
+  [
+    { path: '/', element: <LoginPage /> },
+    {
+      path: '/home',
+      element: (
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      ),
+    },
+  ],
+  {
+    basename: import.meta.env.BASE_URL, // 👈 important line
+  }
+)
